2e913adc8b6f9a96f94034242ef2957717e7213d
Ahmet Eryilmaz <aeryilma@k2m08s08.42kocaeli.com.tr>
2023-07-30 19:26:42 +0300

    30 temmuz

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -8,110 +8,162 @@ import { BallGameModel } from 'src/app/models/model/ballGameModel';
 	styleUrls: ['./game.component.css'],
 })
 export class GameComponent {
-  screenHeight: number;
-  screenWidth: number;
+	screenHeight: number;
+	screenWidth: number;
 
-  @ViewChild('gameCanvas', { static: true }) canvasRef: ElementRef<HTMLCanvasElement>;
-  context: CanvasRenderingContext2D;
+	@ViewChild('gameCanvas', { static: true })
+	canvasRef: ElementRef<HTMLCanvasElement>;
+	context: CanvasRenderingContext2D;
 
-  ball: BallGameModel;
+	ball: BallGameModel;
 
-  paddleHost: PaddleGameModel;
-  paddleGuest: PaddleGameModel;
+	paddleHost: PaddleGameModel;
+	paddleGuest: PaddleGameModel;
 
-  static playerHostScore: number = 0;
-  static playerGuestScore: number = 0;
+	static playerHostScore: number = 0;
+	static playerGuestScore: number = 0;
 
-  isArrowUpPressed: boolean = false;
-  isArrowDownPressed: boolean = false;
+	fixedscreenratio: number;
 
-  constructor() {
-  }
+	isArrowUpPressed: boolean = false;
+	isArrowDownPressed: boolean = false;
 
-  ngAfterViewInit(): void {
-    this.context = this.canvasRef.nativeElement.getContext('2d');
-    this.gameDraw();
-  }
+	constructor() {
+		this.paddleGuest = new PaddleGameModel();
+		this.paddleHost = new PaddleGameModel();
+		this.ball = new BallGameModel();
+	}
 
-  ngOnInit(): void {
-    this.getScreenSize();
-  }
+	ngAfterViewInit(): void {
+		// this.gameDraw();
+		this.gameLoop();
+	}
 
-  private gameDraw(): void {
-    this.context.fillStyle = 'blue';
-    this.canvasRef.nativeElement.height = this.screenHeight - 200;
-    this.canvasRef.nativeElement.width = this.screenWidth - 100;
-    this.context.fillRect(0, 0, 10, 10);
-  }
+	ngOnInit(): void {
+		this.getScreenSize();
+		this.context = this.canvasRef.nativeElement.getContext('2d');
+		this.fixedscreenratio = Math.abs(this.screenWidth / this.screenHeight);
+		console.log('Fixed Screen Ratio : ', this.fixedscreenratio);
+		this.initGameModels();
+		this.gameDraw();
+	}
 
-  gameLoop(): void {
+	//* ^^ eventloophooks and constructor^^
+
+	private gameDraw(): void {
+		this.canvasRef.nativeElement.width = this.screenWidth - 100;
+		this.canvasRef.nativeElement.height = this.screenHeight - 200;
+		this.context.fillStyle = '#000';
+		this.context.fillRect(
+			0,
+			0,
+			this.canvasRef.nativeElement.width,
+			this.canvasRef.nativeElement.height
+		);
+		this.ballDraw();
+		this.playerHostDraw();
+		this.playerGuestDraw();
+	}
+
+	gameLoop(): void {
 		this.gameUpdate();
 		this.gameDraw();
 		setTimeout(() => {
 			window.requestAnimationFrame(this.gameLoop);
 		}, 10);
-  }
+	}
 
-  gameUpdate(): void {
+	initGameModels(): void {
+		//* paddleGuest
+		this.paddleGuest.width = this.fixedscreenratio * 15;
+		this.paddleGuest.height = this.fixedscreenratio * 150;
+		this.paddleGuest.x =
+			this.screenWidth - 100 - this.paddleGuest.width - 2;
+		this.paddleGuest.y = 2;
+		//* paddleHost
+		this.paddleHost.width = this.fixedscreenratio * 15;
+		this.paddleHost.height = this.fixedscreenratio * 150;
+		this.paddleHost.x = 2;
+		this.paddleHost.y = 200;
+		//* ball
+		this.ball.width = this.fixedscreenratio * 10;
+		this.ball.height = this.fixedscreenratio * 10;
+		this.ball.x = 50;
+		this.ball.y = 50;
+	}
 
-  }
+	gameUpdate(): void {}
 
-  paddleUpdateHost(): void {
+	paddleUpdateHost(): void {
+		if (this.isArrowUpPressed) {
+			console.log('deneme');
+			this.paddleHost.y = this.paddleHost.y - this.fixedscreenratio * 2;
+		}
+	}
 
-  }
+	paddleUpdateGuest(): void {}
 
-  paddleUpdateGuest(): void {
+	updateBall(): void {}
 
-  }
+	playerHostDraw(): void {
+		this.context.fillStyle = '#fff';
+		this.context.fillRect(
+			this.paddleHost.x,
+			this.paddleHost.y,
+			this.paddleHost.width,
+			this.paddleHost.height
+		);
+	}
 
-  updateBall(): void {
+	playerGuestDraw(): void {
+		this.context.fillStyle = '#fff';
+		this.context.fillRect(
+			this.paddleGuest.x,
+			this.paddleGuest.y,
+			this.paddleGuest.width,
+			this.paddleGuest.height
+		);
+	}
 
-  }
+	ballDraw(): void {
+		this.context.fillStyle = '#fff';
+		this.context.fillRect(
+			this.ball.x,
+			this.ball.y,
+			this.ball.width,
+			this.ball.height
+		);
+	}
 
-  playerHostDraw(): void {
+	@HostListener('window:keydown', ['$event'])
+	onKeyDown(event: KeyboardEvent) {
+		if (event.key === 'ArrowUp') {
+			this.isArrowUpPressed = true;
+			console.log('arrow up');
+			this.paddleUpdateHost();
+		} else if (event.key === 'ArrowDown') {
+			this.isArrowDownPressed = true;
+			console.log('arrow down');
+		}
+	}
 
-  }
+	// Listen for keyup event on the window
+	@HostListener('window:keyup', ['$event'])
+	onKeyUp(event: KeyboardEvent) {
+		if (event.key === 'ArrowUp') {
+			this.isArrowUpPressed = false;
+		} else if (event.key === 'ArrowDown') {
+			this.isArrowDownPressed = false;
+		}
+	}
 
-  playerGuestDraw(): void {
+	@HostListener('window:resize', ['$event'])
+	onResize(event: Event) {
+		this.getScreenSize();
+	}
 
-  }
-
-  ballDraw(): void {
-
-  }
-
-  @HostListener('window:keydown', ['$event'])
-  onKeyDown(event: KeyboardEvent) {
-    if (event.key === 'ArrowUp') {
-      this.isArrowUpPressed = true;
-      console.log("arrow up");
-    } else if (event.key === 'ArrowDown') {
-      this.isArrowDownPressed = true;
-      console.log("arrow down");
-    }
-  }
-
-  // Listen for keyup event on the window
-  @HostListener('window:keyup', ['$event'])
-  onKeyUp(event: KeyboardEvent) {
-    if (event.key === 'ArrowUp') {
-      this.isArrowUpPressed = false;
-    } else if (event.key === 'ArrowDown') {
-      this.isArrowDownPressed = false;
-    }
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event: Event) {
-    this.getScreenSize();
-  }
-
-  getScreenSize() {
-    this.screenWidth = window.innerWidth;
-    this.screenHeight = window.innerHeight;
-  }
+	getScreenSize() {
+		this.screenWidth = window.innerWidth;
+		this.screenHeight = window.innerHeight;
+	}
 }
-/* 
-TODO :
-Game Start Stop Mechanic
-*/
