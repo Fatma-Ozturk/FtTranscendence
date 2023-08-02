b264790a9579ded625ff4f5ad2501a46dbdb2332
Kuvarti <uzali.kafasi@gmail.com>
2023-08-02 16:27:42 +0300

    adding  speedvalue and remove unnessesary things

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,7 +1,6 @@
 import { PaddleGameModel } from './../../models/model/paddleGameModel';
 import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { BallGameModel } from 'src/app/models/model/ballGameModel';
-import { Ball } from './game';
 
 @Component({
 	selector: 'app-game',
@@ -23,9 +22,10 @@ export class GameComponent {
 	paddleHost: PaddleGameModel;
 	paddleGuest: PaddleGameModel;
 
-	static playerHostScore: number = 0;
-	static playerGuestScore: number = 0;
+	playerHostScore: number = 0;
+	playerGuestScore: number = 0;
 
+	speedmultiplier: number = 2;
 	fixedScreenRatio: number;
 
 	isArrowUpPressed: boolean = false;
@@ -36,7 +36,6 @@ export class GameComponent {
 		this.paddleHost = new PaddleGameModel();
 		this.ball = new BallGameModel();
 		this.bendCall = 0;
-		// this.gameLoop();
 	}
 
 	ngAfterViewInit(): void {
@@ -89,7 +88,10 @@ export class GameComponent {
 		this.paddleHost.height = this.fixedScreenRatio * 50;
 		this.paddleHost.x = 2;
 		this.paddleHost.y = (this.canvasRef.nativeElement.height / 2) - (this.paddleHost.height / 2);
-		//* ball
+		this.ballInit();
+	}
+
+	ballInit(): void {
 		this.ball.width = this.fixedScreenRatio * 5;
 		this.ball.height = this.fixedScreenRatio * 5;
 		this.ball.x = (this.canvasRef.nativeElement.width / 2) - (this.ball.width / 2);
@@ -97,6 +99,7 @@ export class GameComponent {
 		//TODO below parts changed after gamestart call, just random start position for the start
 		this.ball.xVel = Math.floor(Math.random() * 100) % 2 == 0 ? 1 : -1;
 		this.ball.yVel = Math.floor(Math.random() * 100) % 2 == 0 ? 1 : -1;
+		console.log('Scores : ' + 'Host: ' + this.playerHostScore + '\t Guest: ' + this.playerGuestScore);
 	}
 
 	gameUpdate(): void {
@@ -106,12 +109,12 @@ export class GameComponent {
 
 	paddleUpdateHost(): void {
 		if (this.isArrowUpPressed) {
-			this.paddleHost.y = this.paddleHost.y - this.fixedScreenRatio * 2;
+			this.paddleHost.y = this.paddleHost.y - this.speedmultiplier;
 			if (this.paddleHost.y < 0)
 				this.paddleHost.y = 2
 		}
 		if (this.isArrowDownPressed) {
-			this.paddleHost.y = this.paddleHost.y + this.fixedScreenRatio * 2;
+			this.paddleHost.y = this.paddleHost.y + this.speedmultiplier;
 			if (this.paddleHost.y + this.paddleHost.height > this.canvasRef.nativeElement.height)
 				this.paddleHost.y = this.canvasRef.nativeElement.height - this.paddleHost.height - 2;
 		}
@@ -120,35 +123,31 @@ export class GameComponent {
 	paddleUpdateGuest(): void { }//TODO: COMES FROM BACKEND :>
 
 	updateBall = (): void => {
-		if (this.ball.y - (this.fixedScreenRatio * 2) < 0) //* UP Border
+		if (this.ball.y - this.speedmultiplier < 0) //* UP Border
 			this.ball.yVel = 1;
-		if (this.ball.y + (this.fixedScreenRatio * 2) + this.ball.height > this.canvasRef.nativeElement.height) //* DOWN Border
+		if (this.ball.y + this.speedmultiplier + this.ball.height > this.canvasRef.nativeElement.height) //* DOWN Border
 			this.ball.yVel = -1;
 		if (this.ball.x < this.paddleHost.width) { //* LEFT Border
 			if (this.paddleHost.y < this.ball.y && this.paddleHost.y + this.paddleHost.height > this.ball.y)
 				this.ball.xVel = 1;
 			else {
-				// * I Cant Call static variables
-				this.ball.x = (this.canvasRef.nativeElement.width / 2) - (this.ball.width / 2);
-				this.ball.y = 50;
-				this.ball.xVel = Math.floor(Math.random() * 100) % 2 == 0 ? 1 : -1;
-				this.ball.yVel = Math.floor(Math.random() * 100) % 2 == 0 ? 1 : -1;
+				//todo: API CALL with reset game objects!
+				this.playerGuestScore++;
+				this.ballInit();
 			}
 		}
 		if (this.ball.x + this.ball.width > this.canvasRef.nativeElement.width - this.paddleGuest.width - 2) { // * RIGHT Border
 			if (this.ball.y > this.paddleGuest.y && this.ball.y < this.paddleGuest.y + this.paddleGuest.height)
 				this.ball.xVel = -1;
 			else {
-				// * I Cant Call static variables
-				this.ball.x = (this.canvasRef.nativeElement.width / 2) - (this.ball.width / 2);
-				this.ball.y = 50;
-				this.ball.xVel = Math.floor(Math.random() * 100) % 2 == 0 ? 1 : -1;
-				this.ball.yVel = Math.floor(Math.random() * 100) % 2 == 0 ? 1 : -1;
+				//todo: API CALL with reset game objects!
+				this.playerHostScore++;
+				this.ballInit();
 			}
 		}
 
-		this.ball.x += (this.fixedScreenRatio * 2) * this.ball.xVel;
-		this.ball.y += (this.fixedScreenRatio * 2) * this.ball.yVel;
+		this.ball.x += this.speedmultiplier * this.ball.xVel;
+		this.ball.y += this.speedmultiplier * this.ball.yVel;
 	}
 
 	playerHostDraw(): void {
@@ -209,6 +208,7 @@ export class GameComponent {
 		this.screenWidth = window.innerWidth;
 		this.screenHeight = window.innerHeight;
 	}
+
 	setCanvasSize() {
 		this.canvasRef.nativeElement.width = this.screenWidth - 100;
 		this.canvasRef.nativeElement.height = this.screenHeight - 200;
@@ -217,6 +217,7 @@ export class GameComponent {
 		let fixedHeightRatio = this.canvasRef.nativeElement.height / 200;
 		this.fixedScreenRatio =
 			fixedWidthRatio < fixedHeightRatio ? fixedWidthRatio : fixedHeightRatio;
+		this.speedmultiplier = this.speedmultiplier * this.fixedScreenRatio;
 		console.log("FixedScreenRatio: ", this.fixedScreenRatio);
 	}
 }
